446ed1d9ddd4d1936b857109dc2026f71fb9bbc0
Jesús Robles García <jesus.robles5186@alumnos.udg.mx>
2020-10-21 22:55:17 -0500

    first try

--- burbuja.go
+++ burbuja.go
@@ -1,9 +1,17 @@
 package main
 
 func Burbuja(s []int64)  {
+	for i := 0; i < len(s); i++ {
+		for j := i + 1; j < len(s); j++ {
+			if s[i] > s[j] {
+				temp := s[i]
+				s[i] = s[j]
+				s[j] = temp
+			}
+		}
+	}
 
 }
 
 func main()  {
-	
 }
